5f7190994b5da75cb165b97a34982ffaa9a0a555
Gabriel Moura <162451458+MouraGabriel53@users.noreply.github.com>
2024-06-12 08:31:25 -0300

    Delete Scream directory

--- Scream/Database.py
+++
@@ -1,9 +0,0 @@
-#Install -> pip install pyqt6
-
-from PyQt6.QtWidgets import QApplication, QWidget
-
-app = QApplication([])
-janela = QWidget()
-janela.setWindowTitle('Primeira Tela')
-janela.show()
-app.exec()
--- Scream/Insert_Str.py
+++
@@ -1,9 +0,0 @@
-#Install -> pip install pyqt6
-
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
-
-app = QApplication([])
-window = QWidget()
-label = QLabel('Hello World', window) #Insert string
-window.show()
-app.exec()
